cc1aebd7b01d3c403af1084086649367c1ed260d
Jordan Mack <jordan.mack@brilliantnotion.com>
2019-12-07 20:40:34 -0800

    Add rotating screenshot background.

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,10 @@ module.exports =
 				[
 					MiniCssExtractPlugin.loader,
 					// "style-loader",
-					"css-loader",
+					{
+						loader: "css-loader",
+						options: { url: false }
+					},
 					"sass-loader",
 				],
 			},
