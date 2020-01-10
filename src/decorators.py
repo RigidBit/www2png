6724bb3c38073c000cf53e4f1f6ca5173fcb264d
Jordan Mack <jordan.mack@brilliantnotion.com>
2020-01-10 10:18:09 -0800

    Fix error with decorators.

--- src/decorators.py
+++ src/decorators.py
@@ -2,10 +2,21 @@
 Decorators for the web and API endpoints.
 """
 
+from flask import request
 from functools import wraps
 import json
+import os
 
 import database as db
+
+def admin_required(func):
+	"""Guard to ensure a valid admin password is provided."""
+	def wrapper(*args, **kwargs):
+		if not request.authorization or request.authorization.password != os.getenv("WWW2PNG_ADMIN_PASSWORD"):
+			return ("Could not authenticate!", 401, {"WWW-Authenticate": "Basic realm=\"Login Required\""})
+		return func(*args, **kwargs)
+	wrapper.__name__ = func.__name__
+	return wrapper
 
 def api_key_required(func):
 	"""Guard to ensure a valid API key is provided."""
@@ -13,8 +24,9 @@ def api_key_required(func):
 	def wrapper(api_key, *args, **kwargs):
 		connection = db.connect()
 		if not db.check_api_key_exists(connection, api_key):
-			return (json.dumps({"error": "Invalid API Key provided."}), 403)
+			return ({"error": "Invalid API Key provided."}, 403)
 		return func(api_key, *args, **kwargs)
+	wrapper.__name__ = func.__name__
 	return wrapper
 
 def request_id_required(func):
@@ -23,8 +35,9 @@ def request_id_required(func):
 	def wrapper(request_id, *args, **kwargs):
 		connection = db.connect()
 		if not db.get_data_record_by_request_id(connection, request_id):
-			return (json.dumps({"error": "Request ID is not valid."}), 404)
+			return ({"error": "Request ID is not valid."}, 404)
 		return func(request_id, *args, **kwargs)
+	wrapper.__name__ = func.__name__
 	return wrapper
 
 def api_key_and_request_id_required(func):
@@ -33,8 +46,9 @@ def api_key_and_request_id_required(func):
 	def wrapper(api_key, request_id, *args, **kwargs):
 		connection = db.connect()
 		if not db.check_api_key_exists(connection, api_key):
-			return (json.dumps({"error": "Invalid API Key provided."}), 403)
+			return ({"error": "Invalid API Key provided."}, 403)
 		if not db.get_data_record_by_request_id(connection, request_id):
-			return (json.dumps({"error": "Request ID is not valid."}), 404)
+			return ({"error": "Request ID is not valid."}, 404)
 		return func(api_key, request_id, *args, **kwargs)
+	wrapper.__name__ = func.__name__
 	return wrapper
